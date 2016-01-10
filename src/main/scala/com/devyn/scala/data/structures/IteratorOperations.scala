9aca69ec7e85019cf732ef90e0fe5c71986385a9
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2016-01-10 16:25:02 -0600

    add some comments

--- src/main/scala/com/devyn/scala/data/structures/IteratorOperations.scala
+++ src/main/scala/com/devyn/scala/data/structures/IteratorOperations.scala
@@ -31,16 +31,28 @@ class IteratorOperations {
   }
 
   /**
-    *
-    * @param iterator
-    * @param value
-    * @tparam T
-    * @return
+    * find a value in an iterator
+    * @param iterator the iterator to search
+    * @param value the value to search for
+    * @tparam T the type of value
+    * @return an Option with the value if found, None otherwise
     */
   def findValue[T](iterator: Iterator[T], value: T): Option[T] = {
     null
   }
 
+  /**
+    * find an instance of NotACaseClass in the iterator
+    *
+    * Note: You will not be able to get away with simple object level equality comparison.
+    *   You will have to compare the individual values in the object itself,
+    *     or implement equals(object) on NotACaseClass
+    *
+    * @param iterator the iterator to search
+    * @param value an instance of NotACaseClass
+    * @return an option with the value if found, None otherwise
+    *
+    */
   def findNonCaseClass(iterator: Iterator[NotACaseClass], value: NotACaseClass): Option[NotACaseClass] = {
     null
   }
